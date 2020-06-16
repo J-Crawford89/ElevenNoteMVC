0dba0eca190260c747d23f5f4aae11155d21738a
J-Crawford89 <J.Crawford0120@Gmail.com>
2020-06-16 15:24:06 -0400

    Add project files.

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,83 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.NoteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        private ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity = new Note()
+            {
+                OwnerId = _userId,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+            _ctx.Notes.Add(entity);
+
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            var noteArray = _ctx.Notes
+                .Where(e => e.OwnerId == _userId)
+                .Select(e => new NoteListItem
+                {
+                    NoteId = e.NoteId,
+                    Title = e.Title,
+                    CreatedUtc = e.CreatedUtc
+                }).ToArray();
+            return noteArray;
+        }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            var entity = _ctx.Notes.Single(e => e.NoteId == id && e.OwnerId == _userId);
+            return new NoteDetail
+            {
+                NoteId = entity.NoteId,
+                Title = entity.Title,
+                Content = entity.Content,
+                CreatedUtc = entity.CreatedUtc,
+                ModifiedUtc = entity.ModifiedUtc
+            };
+        }
+
+        public bool UpdateNote(NoteEdit model)
+        {
+            var entity = _ctx.Notes.Single(e => e.NoteId == model.NoteId && e.OwnerId == _userId);
+
+            entity.Title = model.Title;
+            entity.Content = model.Content;
+            entity.ModifiedUtc = DateTimeOffset.Now;
+
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public bool DeleteNote(int noteId)
+        {
+            var entity = _ctx.Notes.Single(e => e.NoteId == noteId && e.OwnerId == _userId);
+
+            _ctx.Notes.Remove(entity);
+
+            return _ctx.SaveChanges() == 1;
+        }
+    }
+}
---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,123 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models.NoteModels;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            return new NoteService(userId);
+        }
+
+        // GET: Note
+        public ActionResult Index()
+        {
+            var noteService = CreateNoteService();
+            var model = noteService.GetNotes();
+
+            return View(model);
+        }
+
+        // GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Note/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var noteService = CreateNoteService();
+            if (noteService.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Note could not be created.");
+            return View(model);
+        }
+
+        // GET: Note/Details/{id}
+        public ActionResult Details(int id)
+        {
+            var noteService = CreateNoteService();
+            var model = noteService.GetNoteById(id);
+
+            return View(model);
+        }
+        
+        // GET: Note/Delete/{id}
+        public ActionResult Delete(int id)
+        {
+            var noteService = CreateNoteService();
+            var model = noteService.GetNoteById(id);
+
+            return View(model);
+        }
+
+        // POST: Note/Delete/{id}
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var noteService = CreateNoteService();
+            noteService.DeleteNote(id);
+            TempData["SaveResult"] = "Your note was deleted";
+            return RedirectToAction("Index");
+        }
+
+        // GET: Note/Edit/{id}
+        public ActionResult Edit (int id)
+        {
+            var noteService = CreateNoteService();
+            var detail = noteService.GetNoteById(id);
+            var model = new NoteEdit
+            {
+                NoteId = detail.NoteId,
+                Title = detail.Title,
+                Content = detail.Content
+            };
+            return View(model);
+        }
+
+        // POST: Note/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var noteService = CreateNoteService();
+            if (noteService.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View(model);
+        }
+    }
+}
